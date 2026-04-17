e34da318d558f5a9621f202b154eaa6d4cef18c5
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-17 11:25:13 +0530

    password encoder

--- database/models/user.go
+++ database/models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	Name               string          `json:"name" db:"name"`
 	WhiteListedEmailID entity.Email    `json:"white_listed_email_id" db:"white_listed_email_id"`
 	ImgURL             string          `json:"img_url" db:"img_url"`
-	Password           entity.Password `json:"-" db:"password"`
+	Password           entity.Password `json:"password" db:"password"`
 	OrgID              string          `json:"org_id" db:"org_id"`
 	Role               string          `json:"role" db:"role"`
 	HasAccess          bool            `json:"has_access" db:"has_access"`
@@ -19,7 +19,4 @@ type User struct {
 	IsVerified         bool            `json:"is_verified" db:"is_verified"`
 	CreatedAt          time.Time       `json:"created_at" db:"created_at"`
 	UpdatedAt          time.Time       `json:"updated_at" db:"update_at"`
-}
-
-func main() {
 }
