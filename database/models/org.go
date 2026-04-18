d6a2642abb11da59382a0aaafb61296828c6b4aa
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-19 00:45:45 +0530

    added unique key for the row

--- database/models/org.go
+++ database/models/org.go
@@ -9,7 +9,7 @@ import (
 type Org struct {
 	ID           string       `json:"id" db:"id"`
 	Name         string       `json:"name" db:"name"`
-	Code         string       `json:"code" db:"code"`
+	Code         string       `json:"code" db:"code" unique:"true"`
 	ContactEmail entity.Email `json:"contact_email" db:"contact_email"`
 	LogoURL      string       `json:"logo_url" db:"logo_url"`
 	RootUserID   string       `json:"root_user_id" db:"root_user_id"`
